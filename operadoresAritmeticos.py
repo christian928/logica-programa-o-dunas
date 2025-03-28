75c19a28e5aa739dc505630d84f35b83ac4a0406
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 10:05:25 -0300

    operadoresAritimeticos

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,36 @@
+# adiçaõ
+numero1=10
+numero2=20
+
+soma =numero1 + numero2
+print(soma)
+
+# subtraçao
+
+subtraçao = numero1 - numero2
+print(subtracao)
+
+# multiplicação 
+
+multiplicacao=numero1 * numero2
+print(multiplicacao) 
+
+# divisao
+
+divicao=numero1 / numero2
+print(divicao)
+
+# potencia
+
+potencia=numero1 ** numero2
+print(potencia)
+
+# modulo
+
+modulo=numero1 % numero2
+print(modulo)
+
+# resto da divisao
+
+restodadivicao=numero1 // numero2
+print(restodadivicao)
